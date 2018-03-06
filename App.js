f7a01aa7813a665f0a16e1687a4ca8cf90a8831a
Fransisco Dot <francesco.megna97@gmail.com>
2018-03-06 16:21:39 +0100

    Connected Watchlist to redux

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from 'react-native';
+
+import Watchlist from "./src/screens/watchlist/Watchlist";
+import configureStore from "./src/store/configureStore";
+import * as cryptoAction from "./src/actions/cryptoAction";
+
+const store = configureStore();
+
+store.dispatch(cryptoAction.getCryptoCurrencies());
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <Watchlist />
+      </Provider>
     );
   }
 }
--- src/screens/watchlist/Watchlist.js
+++ src/screens/watchlist/Watchlist.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { connect } from "react-redux";
+import { StyleSheet, View } from "react-native";
+import { List, Text, ListItem } from "native-base";
 
 class Watchlist extends Component {
   render() {
+
     return (
-      <View />
+      <View style={styles.container}>
+        <List dataArray={[]}
+          renderRow={(item) =>
+            (
+              <ListItem>
+                <Text>{item}</Text>
+              </ListItem>
+            )
+          }>
+        </List>
+      </View>
     );
   }
 }
 
-export default Watchlist;
+function mapStateToProps(state) {
+  return {
+    state
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default connect(mapStateToProps)(Watchlist);
